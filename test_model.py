1ab51ddda866a0f5f0a0652f782849526ff218fc
phakawatTER <ter@DESKTOP-04TKP5T.localdomain>
2020-03-16 19:19:39 +0700

    test_model.py can accept video input

--- test_model.py
+++ test_model.py
@@ -5,16 +5,17 @@ import tensorflow as tf
 import cv2
 import numpy as np
 import time
-# from keras.backend.tensorflow_backend import set_session
+# from tensorflow.keras.backend import set_session
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import plot_model
-import keras
+# from tensorflow.keras.tensorflow_backend import set_session
 import os
 import sys
 import math
 from tensorflow.python.client import device_lib
 from scipy.spatial import distance
 import argparse
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-sh", "--input-shape", required=True,
@@ -23,12 +24,20 @@ ap.add_argument("--normalize", required=False,
                 default=False,
                 type=bool, help="is netowrk output normalized?")
 ap.add_argument("-p", "--path", required=True, help="Path to model")
+ap.add_argument("-v","--video-path", required=False,default="",help="Sample VDO for testing")
+ap.add_argument("--use-gpu",required=False,default=False,type=bool,help="Use GPU for computation")
 args = vars(ap.parse_args())
 path_to_model = args["path"]
 input_shape = args["input_shape"]
 normalize = args["normalize"]
-os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
-model = load_model(path_to_model)  # load model
+path_to_vdo  = args["video_path"]
+use_gpu = args["use_gpu"]
+if use_gpu:
+    device = '/gpu:0'
+else:
+    device = '/cpu:0'
+with tf.device(device):
+    model = load_model(path_to_model)  # load model
 plot_model(model, to_file='model.png')  # model schematic
 model_image = cv2.imread("model.png")  # read model image
 mHeight, mWidth, _ = model_image.shape  # get shape of model image
@@ -39,13 +48,15 @@ net = cv2.dnn.readNetFromTensorflow(
     './opencv_face_detector_uint8.pb', './opencv_face_detector.pbtxt')
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-cap = cv2.VideoCapture(0)
+if path_to_vdo == "":
+    cap = cv2.VideoCapture(0)
+else:
+    cap = cv2.VideoCapture(path_to_vdo)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 
 def adjust_gamma(image, gamma=1.0):
-
     invGamma = 1.0 / gamma
     table = np.array([((i / 255.0) ** invGamma) * 255
                       for i in np.arange(0, 256)]).astype("uint8")
@@ -79,17 +90,6 @@ def detect_points(face):
     label_points = (np.squeeze(y_test))
     stop = time.time()
     return label_points
-
-
-def draw_bounding_cube(frame, coords, direction_vector):
-    MAGNITUDE = 200
-    """0,32"""
-    PADDING = 25
-    padding_factor = 0.5
-    face_start_x, face_start_y = coords[0]
-    face_stop_x, face_stop_y = coords[32]
-    _, chin_y = coords[16]
-
 
 def calculate_mar(key_points):  # calculate mouth aspect ratio
     x_points = key_points[::2]
@@ -157,7 +157,7 @@ def get_3d_direction(frame, start, stop, x_points, y_points):
     PADDING = 32
     min_y = min(y_points)
     max_y = max(y_points)
-    print(min_y, max_y)
+    # print(min_y, max_y)
     center_y = int((min_y+max_y)/2 - PADDING)
     # Y-AXIS coordinates
     Y_start_x = int(x1+width+PADDING/2)
@@ -229,7 +229,6 @@ def draw_face_estimator(frame, key_points, draw_index=False, draw_point=True, dr
         elif i in range(96, 98):  # pupils
             COLOR = (0, 255, 0)
         cv2.circle(frame, (x, y), 0, COLOR, 2)
-
         if draw_index:
             cv2.putText(frame, str(i), (x, y), cv2.FONT_HERSHEY_SIMPLEX,
                         0.2, (255, 255, 255), 1, cv2.LINE_AA)
@@ -247,8 +246,8 @@ def draw_face_estimator(frame, key_points, draw_index=False, draw_point=True, dr
 
     width = int((x2-x1)/2)
     height = int((y2-y1)/2-PADDING/2)
-    direction_vector = get_3d_direction(
-        frame, (x1, y1), (width, height), x_points, y_points)
+    # direction_vector = get_3d_direction(
+    #     frame, (x1, y1), (width, height), x_points, y_points)
     # draw_bounding_cube(frame, coords, direction_vector)
     return frame
 
@@ -267,10 +266,6 @@ def show_face(face, key_points):
     y_points = y_points.astype(int)
     coords = list(zip(x_points, y_points))
     frame = np.zeros((height+PADDING, width+PADDING, 3))
-    # cv2.line(frame, (0, y_points[54]),
-    #          (width+PADDING, y_points[54]), (0, 0, 255), 1)
-    # cv2.line(frame, (x_points[54], 0),
-    #          (x_points[54], height+PADDING), (0, 0, 255), 1)
     for i, coord in enumerate(coords, start=0):
         x, y = coord
         COLOR = (255, 133, 71)  # face region
@@ -338,7 +333,6 @@ while True:
                     offest_y = (width - height)/2
                     offest_y = int(offest_y)+10
         face = sharper_frame[y1:y2, x1:x2]  # crop only face
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         # convert color of face image
         grey_face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
         grey_face = cv2.resize(grey_face, (input_shape, input_shape),
@@ -348,13 +342,13 @@ while True:
                             draw_point=True, draw_contour=True)
         MAR = calculate_mar(key_points)
         L_EAR, R_EAR, AVG_EAR = calculate_ear(key_points)
-        print(MAR, L_EAR, R_EAR, AVG_EAR)
+        # print(MAR, L_EAR, R_EAR, AVG_EAR)
         overlay = show_face(face, key_points)
         o_height, o_width, _ = overlay.shape
         frame[0:o_height, WIDTH-o_width:WIDTH] = overlay
 
     except Exception as err:
-        print(err)
+        # print(err)
         pass
     cv2.imshow("frame", frame)
     key = cv2.waitKey(1) & 0xff
